38cafc309803dc8fc168ff7499439db480228226
Miguel Angel Baco Morillo <m.a.bacomorillo@gmail.com>
2021-02-23 22:01:03 +0100

    Templates respuestas y preguntas

--- src/app/trivial/trivial.component.ts
+++ src/app/trivial/trivial.component.ts
@@ -15,6 +15,13 @@ export class TrivialComponent implements OnInit {
   public myForm: FormGroup;
   public myForm2: FormGroup;
   public datos: Array<any> = [];
+  public preguntactual: string = "";
+  public respuestacierta: string = "";
+  public respuestasrandom: Array<string> = [];
+  public indice: number = 0;
+  public notafinal: number = 0;
+  public muestrapregunta: boolean = true;
+  public finalisimo: boolean = false;
 
   constructor(private _peticiones: PreguntasService, public fb: FormBuilder) {
     this.trivif = new Trivial(10,"");
@@ -33,23 +40,74 @@ export class TrivialComponent implements OnInit {
         for (let resultados of data.results) {
           this.trivif.preguntas.push(resultados.question);
           this.trivif.respuestas.push(resultados.correct_answer);
+          this.trivif.respuestasmal[this.trivif.respuestasmal.length] = [resultados.incorrect_answers];
         }
+        this.getPregunta();
       }, error => {
         var errorMessage = error as any;
         console.log(errorMessage);
       });    
   }
 
-  validartrivial() {
-    let i:number = 1;
-    for(let pregunta of this.trivif.preguntas) {
-      let nameformg = "respuesta" + i;
-      this.myForm2 = this.fb.group({
-        nameformg: ['', [Validators.pattern('^' + this.trivif.respuestas[i-1] + '$')]]
-      });
-      i++;
+  getPregunta() {
+    this.respuestasrandom = [];
+    this.muestrapregunta = true;
+
+    //console.log(this.trivif.respuestasmal[this.indice]);
+    if(this.trivif.respuestasmal[this.indice][0].length == 1) {
+      this.respuestasrandom.push(this.remplazarcomillas(this.trivif.respuestas[this.indice]), this.remplazarcomillas(this.trivif.respuestasmal[this.indice][0][0]));
+    } else {
+      this.respuestasrandom.push(this.remplazarcomillas(this.trivif.respuestas[this.indice]), this.remplazarcomillas(this.trivif.respuestasmal[this.indice][0][0]), this.remplazarcomillas(this.trivif.respuestasmal[this.indice][0][1]), this.remplazarcomillas(this.trivif.respuestasmal[this.indice][0][2]));
     }
-    return false;
+
+    this.preguntactual = this.remplazarcomillas(this.trivif.preguntas[this.indice]);
+    this.respuestacierta = this.remplazarcomillas(this.trivif.respuestas[this.indice]);
+
+    this.respuestasrandom.sort(() => Math.random() - 0.5);
+
+    this.myForm2 = this.fb.group({
+      'respuestadada': ['', [Validators.pattern('^' + this.respuestacierta + '$')]]
+    });
+
+    this.indice++;
+    return true;
+  }
+
+  getAnswer() {
+    this.muestrapregunta = false;
+
+    if(this.indice == (this.trivif.preguntas.length)) {
+      this.finalisimo = true;
+    }
+  }
+
+  subirnota() {
+    this.notafinal++;
+    return true;
+  }
+
+  remplazarcomillas(text: string): string {
+    return text.replace(/&amp;/g, "&")
+              .replace(/&gt;/g, ">")
+              .replace(/&lt;/g, "<")
+              .replace(/&quot;/g, '"')
+              .replace(/&#039;/g, "'")
+              .replace(/&ntilde;/g, "ñ")
+              .replace(/&Ntilde;/g, "Ñ")
+              .replace(/&aacute;/g, "á")
+              .replace(/&eacute;/g, "é")
+              .replace(/&iacute;/g, "í")
+              .replace(/&oacute;/g, "ó")
+              .replace(/&uacute;/g, "ú")
+              .replace(/&Aacute;/g, "Á")
+              .replace(/&Eacute;/g, "É")
+              .replace(/&Iacute;/g, "Í")
+              .replace(/&Oacute;/g, "Ó")
+              .replace(/&Uacute;/g, "Ú")
+              .replace(/&euro;/g, "€")
+              .replace(/&deg;/g, "°");
+
+    
   }
 
 }
--- src/app/trivial/trivial.ts
+++ src/app/trivial/trivial.ts
@@ -2,6 +2,10 @@ export class Trivial {
   public category:Array<string> = [];
   public preguntas:Array<string> = [];
   public respuestas:Array<string> = [];
+  public respuestasmal: Array<Array<string>> =[];
+  public numpreguntas: number = 0;
+
   constructor(public npreguntas:number, public buscategory: string) {
+    this.numpreguntas = npreguntas;
   }
 }
